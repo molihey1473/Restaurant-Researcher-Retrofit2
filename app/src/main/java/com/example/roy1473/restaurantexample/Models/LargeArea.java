3520a6b0727e190044f4fc0df6115242d93cd17d
Roy1473 <pgajdmga@gmail.com>
2017-04-09 02:34:43 +0900

    first commit

---
+++ app/src/main/java/com/example/roy1473/restaurantexample/Models/LargeArea.java
@@ -0,0 +1,44 @@
+package com.example.roy1473.restaurantexample.Models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Roy1473 on 2017/03/19.
+ */
+
+public class LargeArea {
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("catch")
+    @Expose
+    private String _catch;
+    @SerializedName("code")
+    @Expose
+    private String code;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCatch() {
+        return _catch;
+    }
+
+    public void setCatch(String _catch) {
+        this._catch = _catch;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
---
+++ app/src/main/java/com/example/roy1473/restaurantexample/Models/Mobile.java
@@ -0,0 +1,33 @@
+package com.example.roy1473.restaurantexample.Models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Roy1473 on 2017/03/19.
+ */
+
+public class Mobile {
+    @SerializedName("l")
+    @Expose
+    private String l;
+    @SerializedName("s")
+    @Expose
+    private String s;
+
+    public String getL() {
+        return l;
+    }
+
+    public void setL(String l) {
+        this.l = l;
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+}
