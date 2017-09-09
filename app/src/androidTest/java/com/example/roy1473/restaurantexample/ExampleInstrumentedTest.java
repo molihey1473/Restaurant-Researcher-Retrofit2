403ec59b05213b16b8f9323ee80e52d791f45d3f
Roy1473 <pgajdmga@gmail.com>
2017-09-10 03:11:00 +0900

    New commit

--- app/src/androidTest/java/com/example/roy1473/restaurantexample/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/roy1473/restaurantexample/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.roy1473.restaurantexamle;
+package com.example.roy1473.restaurantexample;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
